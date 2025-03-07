a7f8baf0f00145d9f7fb3cc744b196260dc82f2f
jdotglove <glover.jarod@gmail.com>
2025-03-06 22:03:58 -0600

    AAA-6: second attempt at fix text color bug

---
+++ coding_challenges/challenge-one/cmd/ccwc.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func Count(filePath string) (result string) {
+
+}
---
+++ coding_challenges/challenge-one/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "challenge-one/cmd"
+
+func main() {
+	cmd.Execute()
+}
